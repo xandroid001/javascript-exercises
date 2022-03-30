bdf84b9909b4de72aeddca45089c072c9395635f
Alexander <alexander.soriano001@gmail.com>
2022-03-30 13:08:40 +0800

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word,number) {
+    let words='';
+    if (number<0){
+        return 'ERROR'
+    }
+    for (i=0; i<number; i++){
+        words = words+word;
+    } return words
 };
 
 // Do not edit below this line
