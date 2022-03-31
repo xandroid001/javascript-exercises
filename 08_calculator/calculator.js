93575044341a13fab8916c1ca6274083622419f0
Alexander <alexander.soriano001@gmail.com>
2022-03-31 15:58:16 +0800

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total=0;
+  for(elem of array){
+    total += elem;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product=1;
+  for (elem of array){
+    product *= elem;
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial=1;
+  for (i=number; i>0; i--){
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
