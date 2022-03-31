7b1373eba407520c8e735f8ab2fa3d63e439684f
Alexander <alexander.soriano001@gmail.com>
2022-03-31 10:13:55 +0800

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let sum=0;
+    if (typeof(start)=="number" && typeof(end)=="number" && start>0 && end>0){
+        start1 = start;
+        end1 = end;
+        if(start>end){
+            start1 = end;
+            end1 = start;
+        }
+        for(let i=start1; i<=end1; i++){
+            sum += i;
+        }return sum;
+    } else {
+    } return 'ERROR'
 };
 
 // Do not edit below this line
