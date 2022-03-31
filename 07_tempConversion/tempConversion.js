14d19c6acb25afb8662dd00f0f3b9a3c75f747ea
Alexander <alexander.soriano001@gmail.com>
2022-03-31 15:32:02 +0800

    completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  celsius = (fahrenheit-32)*5/9;   
+  return +(Math.round(celsius + "e+1")  + "e-1");
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  fahrenheit = (celsius*9/5)+32;
+  return +(Math.round(fahrenheit + "e+1")  + "e-1");
 };
 
 // Do not edit below this line
