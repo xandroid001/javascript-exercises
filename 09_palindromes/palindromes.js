ddcf8109f862d2c5a407c192de6f3c4cec0589c3
Alexander <alexander.soriano001@gmail.com>
2022-03-31 17:27:43 +0800

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    lowerCase = string.toLowerCase()
+    noSpace = lowerCase.replace(/[^A-Za-z0-9]/g, '');
+    split = noSpace.split('');
+    reverse = split.reverse();
+    palindrome = split.join('');
+    if(noSpace===palindrome){
+        return true;
+    } return false;
 };
 
 // Do not edit below this line
