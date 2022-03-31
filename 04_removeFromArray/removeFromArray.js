fb3b1977231e5455c4178539edad305f02254dcf
Alexander <alexander.soriano001@gmail.com>
2022-03-31 09:34:26 +0800

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...testParam) {
+    newArray = array;
+    for (param of testParam){
+        index = newArray.indexOf(param);
+        if (index<0){
+            continue
+        }
+        newArray.splice(index, 1);
+    } return newArray
 };
 
 // Do not edit below this line
