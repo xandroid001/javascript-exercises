8b5aa1be25f7d89a8339a896bfbe6d9d7499f449
Alexander <alexander.soriano001@gmail.com>
2022-04-01 16:30:29 +0800

    completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-const findTheOldest = function() {
-
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+  
+// break
+const findTheOldest = function(object) {
+    let currentYear = new Date().getFullYear()
+    let oldest=1;
+    let oldestAge=1;
+    for (elem of object){
+        let checkAge = (elem.yearOfDeath||currentYear)-elem.yearOfBirth;
+        if (checkAge<oldestAge){
+            continue
+        }
+        oldestAge = checkAge;
+        oldest = elem
+    }
+    return oldest;
 };
 
 // Do not edit below this line
