6851a4e50d862a1daf48532154847d6ae07dc919
Alexander <alexander.soriano001@gmail.com>
2022-04-01 11:33:22 +0800

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = Number(number);
+    let x = 0;
+    let y = 1;
+    if (number===0){
+        return x;
+    }else if (number===1){
+        return y;
+    }else if (number<0){
+        return 'OOPS'
+    }else if (number>1){
+        for (let i=0; i<number-1; i++){
+            z = x + y;
+            x = y;
+            y = z;
+        }
+    }
+    return z;
 };
 
 // Do not edit below this line
