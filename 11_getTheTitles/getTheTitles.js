276c89937a251b907334e75228ea0d0114f846d9
Alexander <alexander.soriano001@gmail.com>
2022-04-01 12:40:50 +0800

    complete 11_getTheTtiles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    titlesArr=[];
+    for (elem of array){
+        titlesArr.push(elem.title)
+    }
+    return titlesArr;
 };
 
 // Do not edit below this line
