795a0c0bd49490df91df8d55927d6dce051d50a7
Alexander <alexander.soriano001@gmail.com>
2022-05-04 16:19:51 +0800

    updated answer for arrays

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,32 @@ const subtract = function(a,b) {
 	return a-b;
 };
 
-const sum = function(array) {
-	let total=0;
-  for(elem of array){
-    total += elem;
-  }
-  return total;
+// const sum = function(array) {
+// 	let total=0;
+//   for(elem of array){
+//     total += elem;
+//   }
+//   return total;
+// };
+
+const sum = function (array) {
+  return array.reduce((currentTotal,number) => {
+    return number + currentTotal;
+  }, 0)
 };
 
-const multiply = function(array) {
-  let product=1;
-  for (elem of array){
-    product *= elem;
-  }
-  return product
+// const multiply = function(array) {
+//   let product=1;
+//   for (elem of array){
+//     product *= elem;
+//   }
+//   return product
+// };
+
+const multiply = array => {
+  return array.reduce((currentProduct, number) => {
+    return number * currentProduct;
+  }, 1)
 };
 
 const power = function(base, exponent) {
