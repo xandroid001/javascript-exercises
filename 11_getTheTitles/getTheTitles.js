80fdab6356e00614b34717ad306f52de2b1dd0c4
Alexander <alexander.soriano001@gmail.com>
2022-05-05 12:50:13 +0800

    shortened the code for getTheTitles using map array method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,14 @@
-const getTheTitles = function(array) {
-    titlesArr=[];
-    for (elem of array){
-        titlesArr.push(elem.title)
-    }
-    return titlesArr;
-};
+// const getTheTitles = function(array) {
+//     titlesArr=[];
+//     for (elem of array){
+//         titlesArr.push(elem.title)
+//     }
+//     return titlesArr;
+// };
+
+const getTheTitles = function(object) {
+    return object.map(book => book.title)
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
