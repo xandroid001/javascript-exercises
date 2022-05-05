b6ff50361570f818b30e0961955ac1a62d735cda
Alexander <alexander.soriano001@gmail.com>
2022-05-05 12:43:16 +0800

    retried palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,25 @@
-const palindromes = function (string) {
-    lowerCase = string.toLowerCase()
-    noSpace = lowerCase.replace(/[^A-Za-z0-9]/g, '');
+// const palindromes = function (string) {
+//     lowerCase = string.toLowerCase()
+//     noSpace = lowerCase.replace(/[^A-Za-z0-9]/g, '');
+//     split = noSpace.split('');
+//     reverse = split.reverse();
+//     palindrome = split.join('');
+//     if(noSpace===palindrome){
+//         return true;
+//     } return false;
+// };
+
+const palindromes = string => {
+    lowerCase = string.toLowerCase();
+    noSpace = lowerCase.replace(/[^A-Za-z0-9]/g, '')
     split = noSpace.split('');
-    reverse = split.reverse();
-    palindrome = split.join('');
+    split.reverse();
+    palindrome = split.join('')
+    console.log(palindrome);
     if(noSpace===palindrome){
         return true;
     } return false;
-};
+}
 
 // Do not edit below this line
 module.exports = palindromes;
