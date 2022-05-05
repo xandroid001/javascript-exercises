dcf5cede4eb0c8521ed777936c96a4504d61bed0
Alexander <alexander.soriano001@gmail.com>
2022-05-05 13:22:12 +0800

    shortened the code with sort function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,35 +1,27 @@
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
-  
 // break
+// const findTheOldest = function(object) {
+//     let currentYear = new Date().getFullYear()
+//     let oldest=1;
+//     let oldestAge=1;
+//     for (elem of object){
+//         let checkAge = (elem.yearOfDeath||currentYear)-elem.yearOfBirth;
+//         if (checkAge<oldestAge){
+//             continue
+//         }
+//         oldestAge = checkAge;
+//         oldest = elem
+//     }
+//     return oldest;
+// };
+
 const findTheOldest = function(object) {
-    let currentYear = new Date().getFullYear()
-    let oldest=1;
-    let oldestAge=1;
-    for (elem of object){
-        let checkAge = (elem.yearOfDeath||currentYear)-elem.yearOfBirth;
-        if (checkAge<oldestAge){
-            continue
-        }
-        oldestAge = checkAge;
-        oldest = elem
-    }
-    return oldest;
+  const sortAge = object.sort((a,b) => {
+    const currentYear = new Date().getFullYear()
+    aAge = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+    bAge = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+    return bAge - aAge;
+  })
+  return sortAge[0];
 };
 
 // Do not edit below this line
